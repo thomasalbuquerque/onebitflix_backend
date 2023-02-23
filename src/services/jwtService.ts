6e498ae4fbe687b382b1d160205e26d4a47ef3f5
Thomas Albuquerque <tealbthomas@gmail.com>
2023-02-23 10:51:55 -0300

    Adicionando middleware de autorização

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -7,5 +7,9 @@ const secret = 'chave-jwt'
 export const jwtService = {
   signPayload: (payload: string | object | Buffer, expiration: string) => {
     return jwt.sign(payload, secret, { expiresIn: expiration })
+  },
+  
+  verifyToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+    jwt.verify(token, secret, callbackfn)
   }
 }
