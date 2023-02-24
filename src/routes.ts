6a882b5a309124331b91854e756d32f950d55914
Thomas Albuquerque <tealbthomas@gmail.com>
2023-02-24 12:33:21 -0300

    Adicionando tabela favorites e rota de adicionar um favorito (post)

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { authController } from './controllers/authController'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodeControllers'
+import { favoritesController } from './controllers/favoritesController'
 import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
 
 const router = express.Router()
@@ -21,5 +22,7 @@ router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
 router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
+
+router.post('/favorites', ensureAuth, favoritesController.save)
 
 export { router }
